ec386954d06bdfa9f22e1d67f53a8784779f6c6c
Doc15 <54259761+Doc15@users.noreply.github.com>
2021-06-29 11:24:32 +0300

    Add files via upload

---
+++ frmAnalysis.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Dijkstra
+{
+    partial class frmAnalysis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.olvNodes = new BrightIdeasSoftware.FastObjectListView();
+            this.node = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.orderoflabelling = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.finalworkingvalue = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            ((System.ComponentModel.ISupportInitialize)(this.olvNodes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // olvNodes
+            // 
+            this.olvNodes.AllColumns.Add(this.node);
+            this.olvNodes.AllColumns.Add(this.orderoflabelling);
+            this.olvNodes.AllColumns.Add(this.finalworkingvalue);
+            this.olvNodes.CellEditUseWholeCell = false;
+            this.olvNodes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.node,
+            this.orderoflabelling,
+            this.finalworkingvalue});
+            this.olvNodes.Cursor = System.Windows.Forms.Cursors.Default;
+            this.olvNodes.FullRowSelect = true;
+            this.olvNodes.HideSelection = false;
+            this.olvNodes.Location = new System.Drawing.Point(13, 13);
+            this.olvNodes.Margin = new System.Windows.Forms.Padding(4);
+            this.olvNodes.Name = "olvNodes";
+            this.olvNodes.ShowGroups = false;
+            this.olvNodes.Size = new System.Drawing.Size(444, 632);
+            this.olvNodes.TabIndex = 2;
+            this.olvNodes.UseCompatibleStateImageBehavior = false;
+            this.olvNodes.View = System.Windows.Forms.View.Details;
+            this.olvNodes.VirtualMode = true;
+            // 
+            // node
+            // 
+            this.node.AspectName = "name";
+            this.node.Text = "Узел";
+            this.node.Width = 100;
+            // 
+            // orderoflabelling
+            // 
+            this.orderoflabelling.AspectName = "orderOfLabelling";
+            this.orderoflabelling.Text = "Порядок";
+            this.orderoflabelling.Width = 110;
+            // 
+            // finalworkingvalue
+            // 
+            this.finalworkingvalue.AspectName = "workingValue";
+            this.finalworkingvalue.Text = "Значения";
+            this.finalworkingvalue.Width = 120;
+            // 
+            // frmAnalysis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 658);
+            this.Controls.Add(this.olvNodes);
+            this.Name = "frmAnalysis";
+            this.Text = "Анализ";
+            ((System.ComponentModel.ISupportInitialize)(this.olvNodes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BrightIdeasSoftware.FastObjectListView olvNodes;
+        private BrightIdeasSoftware.OLVColumn node;
+        private BrightIdeasSoftware.OLVColumn orderoflabelling;
+        private BrightIdeasSoftware.OLVColumn finalworkingvalue;
+    }
+}
---
+++ frmViewAdjMatrix.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Dijkstra
+{
+    partial class frmViewAdjMatrix
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvwAdjMatrix = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lvwAdjMatrix
+            // 
+            this.lvwAdjMatrix.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lvwAdjMatrix.HideSelection = false;
+            this.lvwAdjMatrix.Location = new System.Drawing.Point(13, 13);
+            this.lvwAdjMatrix.Margin = new System.Windows.Forms.Padding(4);
+            this.lvwAdjMatrix.Name = "lvwAdjMatrix";
+            this.lvwAdjMatrix.Size = new System.Drawing.Size(229, 34);
+            this.lvwAdjMatrix.TabIndex = 1;
+            this.lvwAdjMatrix.UseCompatibleStateImageBehavior = false;
+            this.lvwAdjMatrix.View = System.Windows.Forms.View.Details;
+            // 
+            // frmViewAdjMatrix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 59);
+            this.Controls.Add(this.lvwAdjMatrix);
+            this.Name = "frmViewAdjMatrix";
+            this.Text = "Матрица";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvwAdjMatrix;
+    }
+}
